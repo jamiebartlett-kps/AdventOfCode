aa4b2b1302e6deb1fd8d9e35877f58d9527afe1e
Jamie Bartlett <jamie.bartlett@kps.com>
2024-12-10 10:07:38 +0000

    Day 10

---
+++ 2024/Day10_2.mjs
@@ -0,0 +1,52 @@
+import fs from 'fs';
+import { createGrid, directions, outOfBounds, dedupe } from './utils.mjs';
+
+const fullInput = fs.readFileSync('./Input/Day10.txt', 'utf-8');
+const testInput = `
+89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732`;
+
+const underReview = fullInput;
+const grid = createGrid(underReview, (a) => parseInt(a));
+
+const zeroes = []
+grid.forEach((col, x) => {
+    col.forEach((square, y) => {
+        if (square == 0){
+            zeroes.push({x,y});
+        }
+    })
+});
+
+function findTrailHeads(pos){
+    let trailHeads = [];
+    const square = grid[pos.x][pos.y];
+    const lookingFor = square + 1;
+
+    for (let {x,y} of directions){
+        const newPos = {
+            x: pos.x + x,
+            y: pos.y + y
+        }
+        if (!outOfBounds(newPos, grid)){
+            const newSquare = grid[newPos.x][newPos.y];
+            if (newSquare == lookingFor){
+                if (newSquare == 9){
+                    trailHeads = [...trailHeads, newPos];
+                } else {
+                    trailHeads = [...trailHeads, ...findTrailHeads(newPos)];
+                }
+            }
+        }
+    }
+    return trailHeads;
+}
+
+const tally = zeroes.map((zero) => findTrailHeads(zero)).map((a) => a.length).reduce((acc, count) => acc += count, 0);
+console.log(tally);
---
+++ 2024/utils.mjs
@@ -0,0 +1,32 @@
+export function createGrid(str, operator = (a) => a){
+    const rotated = str.trim().split("\n").map((a) => a.split(""));
+    return rotated[0].map((_, x) => rotated.map(row => operator(row[x])))
+}
+
+export const directions = [
+    {x:0,y:-1},
+    {x:1,y:0},
+    {x:0,y:1},
+    {x:-1,y:0}
+]
+
+export function outOfBounds(pos, grid){
+    return (pos.x < 0 || pos.y < 0 || pos.x >= grid.length || pos.y >= grid[0].length);
+}
+
+export function dedupe(arr){
+    return arr.reduce((acc, item) => {
+        const toAdd = !acc.some((i) => {
+            for (let key of Object.keys(item)){
+                if (i[key] != item[key]){
+                    return false
+                }
+            }
+            return true;
+        });
+        if (toAdd){
+            acc.push(item);
+        }
+        return acc;
+    }, []);
+}
